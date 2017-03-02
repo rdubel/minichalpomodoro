dcb28eef623ce39e864d9fbc6498fc46dce746fc
rruellepetel <ruellepetelromain@gmail.com>
2017-03-02 14:31:36 +0100

    start jquery script

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-    console.log("yeah");
+    $("#addtask").click(function() {
+        var newTask = $("#newtask").val();
+        var listElement = $("<li></li>").addClass("task");
+        listElement.html(newTask);
+        var btn = $("<button> &times;</button>");
+        btn.addClass("close");
+        listElement.append(btn);
+        $("#todo ul").append(listElement);
+    });
+    $(".close").click(function() {
+        $(this).parent().remove();
+    })
 });
